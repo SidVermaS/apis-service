b9d72c2edb769a0ae0ff64fe0d34be86abe7e0c2
SidVermaS <sidvermas1234@gmail.com>
2025-02-03 14:57:27 +0530

    updated the package.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,9 @@ import {defineConfig} from 'tsup'
 
 export default defineConfig({
   format:['cjs','esm'],
-entry:['./src/index.ts'],
-dts: true,
-shims:true,
-skipNodeModulesBundle:true,
-clean:true,
+  entry:['./src/index.ts'],
+  dts: true,
+  shims:true,
+  skipNodeModulesBundle:true,
+  clean:true,
 })
